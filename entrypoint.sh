c80e6a987d67ce0384f6d11d7ec8f1db38991f42
Marcus Noble <github@marcusnoble.co.uk>
2024-05-06 15:23:31 +0100

    Ensure node log output is sent to stdout
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "Starting NodeJS app"
 pm2 start --restart-delay 3000 /app/index.js
+tail -f /root/.pm2/logs/index-out.log > /dev/stdout &
+tail -f /root/.pm2/logs/index-error.log > /dev/stdout &
 echo "NodeJS app started, continuing to start drachtio"
 
 echo "Starting Drachtio"
